e2b53ec7d6db0162fbd82a85030c67f35a68d7e1
michelle-french <80438737+michelle-french@users.noreply.github.com>
2021-09-22 14:28:46 -0500

    submit

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,28 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("index")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (user.getPassword().equals(verify)) {
+            return "redirect:";
+        } else {
+            return "user/index";
+        }
+    }
+}
